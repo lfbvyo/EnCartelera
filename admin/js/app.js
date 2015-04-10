c35929d4dc98b0a879863183bbd967da87394bec
Luis Felipe Barnett Vasquez <Felipe@Luiss-MacBook-Pro-2.local>
2015-04-10 14:47:42 -0600

    Added web menu and mobile sidebar, changed default materialize color

--- admin/js/app.js
+++ admin/js/app.js
@@ -1,9 +1,15 @@
 (function(){
+    startStyles();
+
+
   var currentTitleController = function($scope){
     $scope.title = "Peliculas";
   };
   currentTitleController.$inject = ['$scope'];
   angular.module('adminEnCarteleraApp', [])
     .controller('currentTitleController', currentTitleController);
-  
+    // Starts the styles
+  function startStyles(){
+    $(".button-collapse").sideNav();
+  }
 }()); 
