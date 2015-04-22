465d3374087bfbb07f108d926fa3acbec7c1099b
Felipe Barnett <lfbv.yo@gmail.com>
2015-04-21 18:39:13 -0600

    cambios en el form de new movie

--- admin/js/controllers/movieNewController.js
+++ admin/js/controllers/movieNewController.js
@@ -1,16 +1,29 @@
 (function() {
-    
     var movieNewController = function ($scope, $log, moviesService) {
+        //start materialize styles
+        startStyles();
+
+        $scope.movie = true;
 
         $scope.statusBar = moviesService.status;
         
-        
+        $scope.saveMovie = function () {
+            console.debug($scope.master);
+        };
+
+        function startStyles() {
+            $(document).ready(function() {
+                $('select').material_select();
+            });
+        }
+
         //moviesService.addMovie({ name : "Los Vengadores 2", description:"La Película de Marvel más esperada del 2015", youtube:"tmeOjFno6Do", img:"" }); 
     };
-    
+
     movieNewController.$inject = ['$scope', '$log', 'moviesService'];
 
     angular.module('adminEnCarteleraApp')
       .controller('movieNewController', movieNewController);
+
     
 }());
