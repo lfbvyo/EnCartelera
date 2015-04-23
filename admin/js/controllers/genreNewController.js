8be048a839ec5de0348d1d89d8332fd6dcd07d9f
Felipe Barnett <lfbv.yo@gmail.com>
2015-04-23 14:32:55 -0600

    guardar genero terminado

--- admin/js/controllers/genreNewController.js
+++ admin/js/controllers/genreNewController.js
@@ -1,23 +1,35 @@
 (function() {
     var genreNewController = function ($scope, $log, genresService) {
         $scope.genre = {};
-        var printResult = function(status){
+        // Shows feedback when after aving
+        var saveCallBack = function(status){
             if(status) {
                 $scope.genre = {};
                 $scope.$apply();
+                Materialize.toast('Género guardado.', 4000)
             } else {
                 console.debug("Hubo un error :(");
+                Materialize.toast('Error Guardando.', 4000)
             }
         };
+        // Saves a genre through the genresService 
         $scope.saveGenre = function (genre) {
-            genresService.addGenre(genre, printResult);
+            if (validateGenre(genre)) {
+                genresService.addGenre(genre, saveCallBack);
+            } 
         };
+        // Validates genre required data 
+        function validateGenre(genre) {
+            if (typeof(genre.name) == "undefined" || genre.name == "" ) {
+                Materialize.toast('Nombre no puede ser vacío.', 4000)
+                return false;
+            }
+            return true;
+        }
     };
 
     genreNewController.$inject = ['$scope', '$log', 'genresService'];
 
     angular.module('adminEnCarteleraApp')
       .controller('genreNewController', genreNewController);
-
-    
 }());
